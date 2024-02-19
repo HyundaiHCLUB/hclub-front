1ef42eeecbbbcb622158285dea4017a5791e55c7
smartpodo@kakao.com <smartpodo@kakao.com>
2024-02-20 00:34:52 +0900

    remove: ClubControllers & add ViewController

---
+++ src/main/java/site/hclub/hyndai/controller/ViewController.java
@@ -0,0 +1,34 @@
+package site.hclub.hyndai.controller;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+@Slf4j
+@RestController
+@RequestMapping("/")
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public class ViewController {
+    @GetMapping(value = "main")
+    public ModelAndView home(Locale locale, Model model) {
+        log.info("Welcome home! The client locale is {}.", locale);
+        ModelAndView mav = new ModelAndView();
+        Date date = new Date();
+        DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+
+        String formattedDate = dateFormat.format(date);
+        mav.setViewName("home/home");
+        model.addAttribute("serverTime", formattedDate );
+
+        return mav;
+    }
+}
