4de7d54a31d5f7503623252d7541512c3c79cfac
smartpodo@kakao.com <smartpodo@kakao.com>
2024-03-12 23:25:09 +0900

    refactor: 주석달기

--- src/main/java/site/hclub/hyndai/config/TilesConfig.java
+++ src/main/java/site/hclub/hyndai/config/TilesConfig.java
@@ -6,17 +6,28 @@ import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
 import org.springframework.web.servlet.view.tiles3.TilesView;
 import org.springframework.web.servlet.view.tiles3.TilesViewResolver;
 
+/**
+ * @author 김동욱
+ * @description: Apache Tiles Config File
+ * ===========================
+ * AUTHOR      NOTE
+ * ---------------------------
+ * 김동욱         최초생성
+ * ===========================
+ */
+
 @Configuration
 public class TilesConfig {
     @Bean
-    public TilesConfigurer tilesConfigurer(){
+    public TilesConfigurer tilesConfigurer() {
         final TilesConfigurer configurer = new TilesConfigurer();
-        configurer.setDefinitions(new String[] {"WEB-INF/tiles/tiles.xml"});
+        configurer.setDefinitions(new String[]{"WEB-INF/tiles/tiles.xml"});
         configurer.setCheckRefresh(true);
         return configurer;
     }
+
     @Bean
-    public TilesViewResolver tilesViewResolver(){
+    public TilesViewResolver tilesViewResolver() {
         final TilesViewResolver resolver = new TilesViewResolver();
         resolver.setViewClass(TilesView.class);
         resolver.setOrder(1);
--- src/main/webapp/WEB-INF/views/comp/createTeamOne.jsp
+++ src/main/webapp/WEB-INF/views/comp/createTeamOne.jsp
@@ -1,3 +1,5 @@
+<!-- 작성자 : 김동욱 -->
+
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
